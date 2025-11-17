8476aab1b9bc2cee84013fab7e3f0556234e6ee5
Leandro Rodrigues <leandromartinsr10@hotmail.com>
2025-11-17 12:20:41 -0300

    Teste CI

--- aplicacao/apps.py
+++ aplicacao/apps.py
@@ -6,10 +6,10 @@ class AplicacaoConfig(AppConfig):
     name = 'aplicacao'
 
     def ready(self):
-        
-        # NAO RODA WEBSOCKET NO CELERY
-        if 'celery' in sys.argv[0]: return
 
-        # RODA WEBSOCKET APENAS NO SERVIDOR E SOMENTE DEPOIS DE TUDO CONFIGURADO
-        from .preco_socket import preco_socket
-        preco_socket()
+      # NAO RODA WEBSOCKET NO CELERY
+      if 'celery' in sys.argv[0]: return
+
+      # RODA WEBSOCKET APENAS NO SERVIDOR E SOMENTE DEPOIS DE TUDO CONFIGURADO
+      from .preco_socket import preco_socket
+      preco_socket()

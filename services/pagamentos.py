9f5d0f58be819e698ec08a61dc4d4f22f41c4dd5
Leandro Rodrigues <leandromartinsr10@hotmail.com>
2026-05-13 19:19:48 -0300

    fix: Correções no serviço de pagamento e provider
    Decisões
    - Adicionado tipagem ao provedor de pagamento (idealmente deveria ser interface)
    - Alterado variável no DTO para semântica
    - Criado provedor de pagamento LNMarket responsável pela geração do invoice
    - Provedor é responsável por gravar log sobre qualquer erro e devolver sucesso e dados

--- services/pagamentos.py
+++ services/pagamentos.py
@@ -3,18 +3,19 @@ from dataclasses import dataclass
 from io import BytesIO
 from time import time
 
+from infra.lnmarkets import LNMarket, LNMarketResult
+
 @dataclass
 class PagamentoResult:
-      sucesso: bool
       status_code: int
       mensagem: str | None = None
       qrcode: str | None = None
       id_invoice: str | None = None
-      payment_hash: str | None = None
+      payment_request: str | None = None
 
 class Pagamento:
 
-      def __init__(self, cache, log_user, provider_pagamento) -> None:
+      def __init__(self, cache, log_user, provider_pagamento: LNMarket) -> None:
             self._cache = cache
             self._log_user = log_user
             self._provider_pagamento = provider_pagamento
@@ -26,8 +27,8 @@ class Pagamento:
             preco_em_sats = self._transformar_preco_em_sats(preco_atual_btc)
 
             # 2. Abre invoice pendente - Intenção de fazer pagamento
-            deposito = await self._provider_pagamento.novo_deposito(preco_em_sats=preco_em_sats) # Tenho log aqui dentro? Se não, preciso logar aqui em baixo
-            if not deposito.sucesso: return PagamentoResult(sucesso=False, status_code=deposito.status_code, mensagem=deposito.mensagem)
+            deposito = await self._provider_pagamento.gerar_novo_deposito(preco_em_sats=preco_em_sats)
+            if not deposito.sucesso or not deposito.payment_request: return PagamentoResult(status_code=500, mensagem=f"❌ Erro ao gerar novo depósito.")
 
             # 3. Armazena email + id invoice NÃO PAGO em cache por 30 dias
             invoice = {'username': username, 'id_invoice': deposito.deposit_id, 'timestamp': time() * 1000, 'status': False}
@@ -40,7 +41,7 @@ class Pagamento:
             qrc_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
             self._log_user(username=username).info(f"QRCode Gerado.")
-            return PagamentoResult(sucesso=True, status_code=200, qrcode=qrc_base64, id_invoice=deposito.deposit_id, payment_hash=deposito.payment_request)
+            return PagamentoResult(status_code=200, qrcode=qrc_base64, id_invoice=deposito.deposit_id, payment_request=deposito.payment_request)
 
 
       def _transformar_preco_em_sats(self, preco_atual_btc):

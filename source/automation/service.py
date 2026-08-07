0144b0865a984e042507340a5a264236370c3125
Leandro Rodrigues <leandromartinsr10@hotmail.com>
2026-08-07 13:19:54 -0300

    feat(automation): implement automation service

---
+++ source/automation/service.py
@@ -0,0 +1,43 @@
+from .interfaces import CacheProtocol
+from ..dtos import ConfigurationDTO, ApiDTO
+from ..events import AutomationEvent, Channel
+
+
+class AutomationService:
+
+    def __init__(self, cache_repository: CacheProtocol) -> None:
+        self._cache_repository = cache_repository
+
+
+    def get_automation_overview(self, exchange: str, email: str) -> tuple[ConfigurationDTO, ApiDTO, bool]:
+        configuration = self._cache_repository.get_configuration(exchange, email)
+        api = self._cache_repository.get_api(exchange, email)
+        automation_status = self._cache_repository.get_status_automation(exchange, email)
+        return configuration, api, automation_status
+
+
+    def enable_automation(self, exchange: str, email: str) -> None:
+        """Ativa a automação publicando evento para o websocket começar a coletar/usar dados do usuário na exchange."""
+        self._cache_repository.add_activated_automation(exchange, email)
+        payload = {"type": AutomationEvent.STARTED, "email": email, "exchange": exchange}
+        self._cache_repository.publish_event(Channel.AUTOMATION, payload)
+
+
+    def disable_automation(self, exchange: str, email: str) -> None:
+        """Desativa a automação publicando evento para o webscoket parar de coletar/usar dados do usuário na exchange."""
+        self._cache_repository.remove_activated_automation(exchange, email)
+        payload = {"type": AutomationEvent.STOPPED, "email": email, "exchange": exchange}
+        self._cache_repository.publish_event(Channel.AUTOMATION, payload)
+
+
+    def save_api(self, exchange, email: str, api: ApiDTO):
+        self._cache_repository.save_api(exchange, email, api)
+
+
+    def save_configuration(self, exchange, email: str, configuration: ConfigurationDTO):
+        cached_config = self._cache_repository.get_configuration(exchange, email)
+        cached_config.marginUSD =  configuration.marginUSD
+        cached_config.leverage = configuration.leverage
+        cached_config.buy_variation = configuration.buy_variation
+        cached_config.percentage_profit = configuration.percentage_profit
+        self._cache_repository.save_configuration(exchange, email, configuration)

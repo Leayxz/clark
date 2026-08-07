421b895ac002433ba27c6e0460afc93838e75a78
Leandro Rodrigues <leandromartinsr10@hotmail.com>
2026-08-07 17:53:58 -0300

    feat(config): define application cache keys and constants

---
+++ source/constants.py
@@ -0,0 +1,11 @@
+class CacheKeys:
+    ALL_ACTIVATED_AUTOMATION = "all_activated_automation"
+    AUTOMATION_CONFIGURATION = "automation_configuration"
+    AUTOMATION_API = "automation_api"
+    LNMCREDENTIALS = "lnmcredentials"
+    LNMCONFIGURATION = "lnmconfiguration"
+    THIRTY_DAYS_IN_SECONDS = 60*60*24*30
+
+class Automation:
+    AUTOMATION_PRICE_IN_SATS = 570
+    
